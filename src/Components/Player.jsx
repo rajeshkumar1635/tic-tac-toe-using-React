89d337bd75508ef1a287d771c9eff8ca3d312d29
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2024-05-19 17:00:56 -0400

    created a edit functionality button in Player Component

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 export default function Player({name,symbol}) {
+const [isEditing,setIsEditing]=useState(false);
+function handler(){
+    setIsEditing(true);
+}
+
     return (
 <li>
         <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input type="text" required />:<span className="player-name">{name}</span> }
         <span className="player-symbol">{symbol}</span> 
         </span>
-        <button>Edit</button>
+        <button onClick={()=>handler()}>Edit</button>
       </li>
     );
 }

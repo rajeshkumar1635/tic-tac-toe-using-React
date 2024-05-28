0e336bc2679e1f1f61db0b293ca1a869a50593b8
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2024-05-28 11:24:55 -0400

    added log component

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -5,23 +5,30 @@ const initialGameBoard=[
     [null,null,null],
     [null,null,null]
 ];
-export default function GameBoard({onSelectSquare,activePlayerSymbol}){
-const [gameBoard,setGameBoard]=useState(initialGameBoard);
-
-function handleSelectSquare(rowindex,colindex){
-    setGameBoard((prevgameBoard)=>{
-        const updatedBoard=[...prevgameBoard.map(innerArray=>[...innerArray])];
-   updatedBoard[rowindex][colindex]=activePlayerSymbol;
-return updatedBoard;
-    });
-    onSelectSquare();
+export default function GameBoard({onSelectSquare,turns}){
+    let gameBoard=initialGameBoard;
+for (const turn of turns){
+    const {square,player}=turn;
+    const {row,col}=square;
+    gameBoard[row][col]=player;
 }
+
+// const [gameBoard,setGameBoard]=useState(initialGameBoard);
+
+// function handleSelectSquare(rowindex,colindex){
+//     setGameBoard((prevgameBoard)=>{
+//         const updatedBoard=[...prevgameBoard.map(innerArray=>[...innerArray])];
+//         updatedBoard[rowindex][colindex]=activePlayerSymbol;
+//     return updatedBoard;
+//     });
+//     onSelectSquare();
+// }
     return(
  <ol id="game-board">
     {gameBoard.map((row,rowindex)=><li key={rowindex}>
         <ol>
             {row.map((playerSymbol,colindex)=><li key={colindex}>
-                <button onClick={()=>handleSelectSquare(rowindex,colindex)}>
+                <button onClick={()=> onSelectSquare(rowindex,colindex)}>
                 {playerSymbol}
                 </button>
             </li>)}
